86a68af50957f8b508eac9812071a8e9f90b0041
Sebastian Welch <sebastianwelch@gmail.com>
2018-09-28 11:47:29 -0400

    Sanitized input commands

--- smugbot.js
+++ smugbot.js
@@ -8,38 +8,45 @@ client.on('ready', () => {
 
 // Event listener for messages
 client.on('message', msg => {
-    // Replies to anybody who says "ping" with "pong"
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
-    // Sends the user an URL for their avatar
-    if (msg.content === "what is my avatar"){
-        msg.reply(msg.author.avatarURL);
-    }
-
-    // Sends a picture of Todd Howard in chat
-    if (msg.content === "~todd"){
-        msg.channel.send({files:["./images/todd.jpg"]});
-    }
-
-    // Embed a message
-    if (msg.content === "how to embed"){
-        const embed = new Discord.RichEmbed()
-          .setTitle("A slick little embed")
-          .setColor(0xFF0000)
-          .setDescription("Hello, this is a slick embed!");
-        msg.channel.send(embed);
-    }
-
-    //Clean chat
-    if (msg.content.includes("~clear")){
-        var numMessages = msg.content.split(" ")[1];
-        try{
-            msg.channel.bulkDelete(numMessages);
-            msg.channel.send("Cleaned " + numMessages + " messages.");
+    // Basic bot commands
+    if (msg.content[0] == "~"){
+        var command = msg.content.substr(1);
+        command = command.split(" ");
+        command[0] = command[0].toLowerCase();
+ 
+        // Replies to anybody who says "ping" with "pong"
+        if (command[0] === "ping") {
+            msg.reply('Pong!');
         }
-        catch(err){
-            msg.channel.send("Invalid command.");
+        // Sends the user an URL for their avatar
+        if (command[0] === "what is my avatar"){
+            msg.reply(msg.author.avatarURL);
+        }
+
+        // Sends a picture of Todd Howard in chat
+        if (command[0] === "todd"){
+            msg.channel.send({files:["./images/todd.jpg"]});
+        }
+
+        // Embed a message
+        if (command[0] === "embed"){
+            const embed = new Discord.RichEmbed()
+            .setTitle("A slick little embed")
+            .setColor(0xFF0000)
+            .setDescription("Hello, this is a slick embed!");
+            msg.channel.send(embed);
+        }
+
+        //Clean chat by number of messages
+        if (command[0] === "clear"){
+            var numMessages = command[1];
+            try{
+                msg.channel.bulkDelete(numMessages);
+                msg.channel.send("Cleaned " + numMessages + " messages.");
+            }
+            catch(err){
+                msg.channel.send("Invalid command.");
+            }
         }
     }
 });
